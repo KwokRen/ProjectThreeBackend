ac8e5a9a10d9b256b39afe36c110e97263c05b28
Kwok <kwokrenx@gmail.com>
2020-08-23 12:25:24 -0400

    added another route to get the comments using the video id

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :videos do
+    # we are nesting this resource inside videos only for the /videos/:video_id/comments route
+    resources :comments
   end
   resource :users, only: [:create] do
     resources :comments
