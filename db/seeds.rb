0fe4f79fe002b9070cfb6e35a82533e5b50819ba
J-Morocho <jmorocho95@yahoo.com>
2020-08-26 13:59:01 -0400

    video.create in seeds raises NotNullViolation ERROR

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-Video.create("title": 'Video2', like_count: 102, dislike_count: 2, videoID: 'd9jjojj3j9')
