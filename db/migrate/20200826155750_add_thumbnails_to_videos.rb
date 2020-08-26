6b890559ba6cefaee53da3612a867cf12b672ffd
J-Morocho <jmorocho95@yahoo.com>
2020-08-26 12:10:53 -0400

    add thumbnails to videos

---
+++ db/migrate/20200826155750_add_thumbnails_to_videos.rb
@@ -0,0 +1,9 @@
+class AddThumbnailsToVideos < ActiveRecord::Migration[6.0]
+  def change
+    change_table :videos do |t|
+      t.string :thumb_default
+      t.string :thumb_medium
+      t.string :thumb_high
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_23_203228) do
+ActiveRecord::Schema.define(version: 2020_08_26_155750) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,9 +37,12 @@ ActiveRecord::Schema.define(version: 2020_08_23_203228) do
     t.integer "like_count"
     t.integer "dislike_count"
     t.string "videoID"
-    t.bigint "comment_id"
+    t.bigint "comment_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "thumb_default"
+    t.string "thumb_medium"
+    t.string "thumb_high"
     t.index ["comment_id"], name: "index_videos_on_comment_id"
   end
 
